a1dc6272886bd1770350420ff82dc7f951e3567c
Rodrigo Wright <wright.rodrigo380@gmail.com>
2024-07-29 22:32:29 -0300

    overllaping main

---
+++ src/nonOverlapping/main.py
@@ -0,0 +1,7 @@
+from overlapping import Solution
+def main():
+    #Altere o campo intervals:
+    intervals = [[1,2],[2,3],[3,4],[1,3]]
+    saida = Solution()
+    print ("Saída:",saida.overlapping(intervals))
+main()
