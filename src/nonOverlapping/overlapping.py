6d88649096d44957ebae2fb2286af6ddb2b5cc17
Rodrigo Wright <wright.rodrigo380@gmail.com>
2024-07-29 22:23:21 -0300

    overlapping solution

--- src/nonOverlapping/overlapping.py
+++ src/nonOverlapping/overlapping.py
@@ -1,1 +1,9 @@
-print("Hello world!")
+def eraseOverlapIntervals(intervals):  
+    intervals.sort(key=lambda x: x[1])  
+    count = 1
+    end = intervals[0][1]  
+    for i in range(1, len(intervals)):
+        if intervals[i][0] >= end:
+            count += 1
+            end = intervals[i][1]  
+    return len(intervals) - count
